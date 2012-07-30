34601c8de551bc8cf1e9d7d870b1f69d51ec1872
HVN <hvnsweeting@gmail.com>
2012-07-30 16:28:53 +0700

    init

---
+++ get_bootstrap.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget http://svn.zope.org/*checkout*/zc.buildout/trunk/bootstrap/bootstrap.py
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = "project_name",
+        package_dir = {'':'src'}
+        )
