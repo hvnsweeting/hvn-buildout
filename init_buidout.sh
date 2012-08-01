26844ed7e726d2fba01283db58ad4127e9896cd7
HVN <hvnsweeting@gmail.com>
2012-08-01 09:54:17 +0700

    run buildout

--- init_buidout.sh
+++ init_buidout.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # Download file
+echo "Downloading bootstrap.py..."
 curl -O http://svn.zope.org/*checkout*/zc.buildout/trunk/bootstrap/bootstrap.py
 
 # Run file
+echo "Running bootstrap.py..."
 python bootstrap.py
+
+echo "init buildout..."
+# Init buildout
+bin/buildout
